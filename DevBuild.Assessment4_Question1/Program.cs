29b022d16f99ebe60fc5995637d4dc22b1dc1410
Josh Zimmerman <JoshuaZimmerman@quickenloans.com>
2018-11-02 13:03:11 -0400

    Add project files.

---
+++ DevBuild.Assessment4_Question1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBuild.Assessment4_Question1 {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
