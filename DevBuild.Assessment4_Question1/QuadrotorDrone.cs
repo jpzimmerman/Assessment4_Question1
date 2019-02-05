51d9342c876bf8b59d82a6e1a27faf19e9be7818
Zimmerman <joshuazimmerman@quickenloans.com>
2019-02-05 14:43:38 -0500

    Cleans up Quadrotor drone class to use expression bodies

--- DevBuild.Assessment4_Question1/QuadrotorDrone.cs
+++ DevBuild.Assessment4_Question1/QuadrotorDrone.cs
@@ -7,15 +7,10 @@ using System.Threading.Tasks;
 namespace DevBuild.Assessment4_Question1 {
     class QuadrotorDrone : Drone {
 
-        public QuadrotorDrone() {
-        }
+        public QuadrotorDrone() {}
 
         public QuadrotorDrone(int totalFlyTime, int totalDistance) : base(totalFlyTime, totalDistance) { }
 
-        public override int FlightSpeed(int totalTime1, int totalDistance1) {
-
-            return AddDistance(totalDistance1) / AddFlightTime(totalTime1);
-
-        }
+        public override int FlightSpeed(int totalTime1, int totalDistance1) => AddDistance(totalDistance1) / AddFlightTime(totalTime1);
     }
 }
